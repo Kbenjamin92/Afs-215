c3a68bbe25fdebac4c8cc1632c3c580ecec5add8
kbenjamin92 <kipp.benjamin@bryanuniversity.edu>
2021-04-27 18:01:09 -0400

    Capstone-215

--- week-7/Walmart/walmart.js
+++ week-7/Walmart/walmart.js
@@ -1,28 +1,43 @@
 class myClass {
     constructor (discount) {
         this.itemArr = [];
+        this.priceArr = [];
         this.discount = discount;
     }
 
     addItem(item) {
-        const list = this.itemArr.push(item);
-        return list
+        this.itemArr.push(item);
+        return this.itemArr
     }
 
     addPrice(itemPrice) {
-        
-        return itemPrice
+        this.priceArr.push(itemPrice)
+        return this.priceArr
     }
 
-    calculate(total) {
-
+    calculate() {
+        const price = this.priceArr.toString()
+        const joinArrays = this.itemArr.concat(price)
+        const joinData = joinArrays.join(' ')
+        // this.totalArr.push(joinedData)
+        return joinData
     }
     
 }
 
 let instance = new myClass()
-let item = instance.addItem('food')
-let itemPrice = instance.addPrice(20)
+let item = instance.addItem('milk')
+let itemPrice = instance.addPrice(20.55)
+let total = instance.calculate()
+
+let itemTwo = instance.addItem('Salad')
+let itemPriceTwo = instance.addPrice(10.35)
+let totalTwo = instance.calculate()
 
 console.log(item)
 console.log(itemPrice)
+console.log(total)
+
+console.log(itemTwo)
+console.log(itemPriceTwo)
+console.log(totalTwo)
